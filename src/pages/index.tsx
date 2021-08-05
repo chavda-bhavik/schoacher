f4705ef4b09af6cc8888267a92f91e1e843c3816
chavda-bhavik <bhavik.chvda@gmail.com>
2021-08-05 21:22:44 +0530

    initial setup with next-tailwindcss-husky-typescript-eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface IndexProps {}
+
+const Index: React.FC<IndexProps> = ({}) => {
+    return <p className="text-3xl">Hello</p>;
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
