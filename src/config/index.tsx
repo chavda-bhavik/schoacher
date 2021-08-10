be87ccc88431f4b50b990842c448001c161f8b3c
chavda-bhavik <bhavik.chvda@gmail.com>
2021-08-10 15:16:08 +0530

    finalized landing page for live

---
+++ src/config/index.tsx
@@ -0,0 +1,8 @@
+const siteMetadata = {
+    title: `Schoacher`,
+    description: `Schoacher is a platform where schools can post their requirements for teachers and teachers can apply for jobs`,
+    siteUrl: `https://www.schoacher.com`,
+    image: `/og/default.png`,
+};
+
+export { siteMetadata };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { Selection } from '@/components/Selection';
 import { Curls, Teaching, School, Teacher } from '@/static/SVGs';
 import { useForm } from '@formcarry/react';
 import { Icon } from '@/static/Icons';
+import Image from 'next/image';
+import { SEO } from '@/components/SEO';
 
 interface IndexProps {}
 
@@ -22,6 +24,7 @@ const Index: React.FC<IndexProps> = ({}) => {
 
     return (
         <>
+            <SEO title="Home" description="Connecting Teachers with Schools" />
             <nav id="header" className="w-full z-30 text-white bg-primary-green">
                 <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                     <div className="pl-4 flex items-center">
@@ -29,25 +32,12 @@ const Index: React.FC<IndexProps> = ({}) => {
                             className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
                             href="#"
                         >
-                            <svg
-                                className="h-8 fill-current inline"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 512.005 512.005"
-                            >
-                                <rect
-                                    fill="#2a2a31"
-                                    x="16.539"
-                                    y="425.626"
-                                    width="479.767"
-                                    height="50.502"
-                                    transform="matrix(1,0,0,1,0,0)"
-                                />
-                                <path
-                                    className="plane-take-off"
-                                    d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-                                />
-                            </svg>
-                            LANDING
+                            <Image
+                                src="/Logo-light.png"
+                                width={130}
+                                height={80}
+                                alt="schoacher logo"
+                            />
                         </a>
                     </div>
                 </div>
