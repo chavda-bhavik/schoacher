6c201c27c37bb3c88c806b002d3ce146c6ff13c3
chavda-bhavik <bhavik.chvda@gmail.com>
2021-10-08 19:19:25 +0530

    made applications to fetch live data

--- src/components/employer/applications/ApplicantCard.tsx
+++ src/components/employer/applications/ApplicantCard.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface ApplicantCardProps {}
+interface ApplicantCardProps {
+    id: number;
+    photoUrl?: string;
+    name?: string;
+    headline?: string;
+    about?: string;
+}
 
-export const ApplicantCard: React.FC<ApplicantCardProps> = ({}) => {
+export const ApplicantCard: React.FC<ApplicantCardProps> = ({
+    id,
+    photoUrl,
+    name,
+    headline,
+    about,
+}) => {
     return (
-        <Link href="/employer/view-profile/1">
+        <Link href={`/employer/view-profile/${id}`}>
             <a className="transform duration-500 rounded-md block">
                 <article className="relative group cursor-pointer transition-shadow duration-200 hover:shadow-md">
                     <div className="overflow-hidden rounded-t-md">
@@ -15,21 +27,16 @@ export const ApplicantCard: React.FC<ApplicantCardProps> = ({}) => {
                             height={150}
                             layout="responsive"
                             className="w-full h-auto"
-                            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-                            alt="John Doe"
+                            src={photoUrl}
+                            alt={`Photo of ${name}`}
                         />
                     </div>
                     <div className="p-2 my-auto bg-gray-50 rounded-b-md">
                         <h1 className="text-xl font-semibold text-gray-800 mt-2 group-hover:underline transition-all duration-200">
-                            John Doe
+                            {name}
                         </h1>
-                        <h1 className="text-lg font-medium text-gray-700 mt-1">
-                            School Teacher at Saraswati Vidhyalaya
-                        </h1>
-                        <p className="text-base text-gray-500 mt-2">
-                            Super creative and colorful illustrations by Matheus Lopes. Check out
-                            more of his amazing artworks in his portfolio.
-                        </p>
+                        <h1 className="text-lg font-medium text-gray-700 mt-1">{headline}</h1>
+                        <p className="text-base text-gray-500 mt-2">{about}</p>
                         <p className="text-base mt-3 text-blue-500 font-medium group-hover:underline transition-all duration-200">
                             View Profile
                         </p>
--- src/pages/employer/applications.tsx
+++ src/pages/employer/applications.tsx
@@ -1,39 +1,149 @@
-import React from 'react';
-import { Input } from '@/components/Input';
+import React, { useEffect, useState } from 'react';
+import client from '@/apollo-client';
+import { GetServerSidePropsResult } from 'next';
+
+import constants from '@/shared/constants';
 import { EmployerTopbar } from '@/components/Topbar';
 import { ApplicantCard } from '@/components/employer/applications/ApplicantCard';
+import { ApplicationsFilterType, RequirementType, Application } from '@/interfaces';
+import { IconButton } from '@/components/IconButton';
+import { Filter } from '@/components/teacher/Filter';
+import { Backdrop } from '@/components/Backdrop';
+import { ApplicationFilterForm } from '@/components/employer/applications/ApplicationFilterForm';
 
-interface ApplicationsProps {}
+import {
+    GET_ALL_REQUIREMENTS,
+    getAllRequirements,
+    getAllRequirementsVariables,
+    GET_APPLICATIONS,
+    getApplications,
+    getApplicationsVariables,
+} from '@/graphql/employer/query';
+import { useLazyQuery } from '@apollo/client';
+import { Wrapper } from '@/components/Wrapper';
 
-const Applications: React.FC<ApplicationsProps> = ({}) => {
+interface ApplicationsProps {
+    requirements: RequirementType[];
+}
+
+const Applications: React.FC<ApplicationsProps> = ({ requirements }) => {
+    const [getApplications, { loading, data }] = useLazyQuery<
+        getApplications,
+        getApplicationsVariables
+    >(GET_APPLICATIONS);
+    const [showFiltersModal, setShowFiltersModal] = useState(false);
+    const [filters, setFilters] = useState<ApplicationsFilterType>();
+    const [applications, setApplications] = useState<Application[]>();
+
+    useEffect(() => {
+        getApplications({
+            variables: {
+                employerId: constants.employerId,
+            },
+        });
+    }, [getApplications]);
+
+    useEffect(() => {
+        if (!loading && data) setApplications(data.applications);
+    }, [loading, data]);
+
+    const onFiltersModalShow = () => {
+        setShowFiltersModal(true);
+    };
+    const onFiltersModalClose = () => {
+        setShowFiltersModal(false);
+    };
+
+    const onFiltersSubmit = (filtersData: ApplicationsFilterType) => {
+        getApplications({
+            variables: {
+                employerId: Number(constants.employerId),
+                requirementId: Number(filtersData.requirementId),
+            },
+        });
+        setFilters(filtersData);
+        setShowFiltersModal(false);
+    };
+
+    let filtersContent = [];
+    if (filters) {
+        if (filters.requirementId)
+            filtersContent.push(
+                <Filter
+                    key="Requiremet"
+                    title="Requirement"
+                    text={filters.requirementId.toString()}
+                />
+            );
+    }
+
     return (
         <div className="employer-section">
             <EmployerTopbar />
-            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-                <div className="px-2 py-3 grid grid-cols-2">
-                    <Input id="application" name="application" type="select" className="w-max">
-                        <option>English Teacher for Standard 11 &amp; 12</option>
-                        <option>Nursery Teacher</option>
-                    </Input>
-                    <Input id="status" name="status" type="select" className="w-max ml-auto">
-                        <option>All</option>
-                        <option>Applied</option>
-                        <option>Accepted</option>
-                        <option>Rejected</option>
-                    </Input>
+            <section className="section">
+                <div className="section-header sticky top-0 z-50 bg-dustWhite">
+                    <div className="flex flex-row w-full space-x-2">
+                        <IconButton
+                            icon="filter"
+                            size="sm"
+                            variant="bordered"
+                            className="flex-shrink-0 self-center"
+                            onClick={onFiltersModalShow}
+                        />
+                        <div className="overflow-x-auto flex-grow flex flex-row space-x-2">
+                            {filtersContent}
+                        </div>
+                    </div>
                 </div>
                 <div className="border-t border-gray-200">
                     <div className="grid grid-cols-4 gap-2 p-4">
-                        <ApplicantCard />
-                        <ApplicantCard />
-                        <ApplicantCard />
-                        <ApplicantCard />
-                        <ApplicantCard />
+                        <Wrapper loading={loading}>
+                            {applications &&
+                                applications.map((application) => (
+                                    <ApplicantCard
+                                        id={application.id}
+                                        key={application.id}
+                                        about={application.teacher.about}
+                                        headline={application.teacher.headline}
+                                        name={`${application.teacher.firstName} ${application.teacher.lastName}`}
+                                        photoUrl={application.teacher.photoUrl}
+                                    />
+                                ))}
+                        </Wrapper>
                     </div>
                 </div>
-            </div>
+            </section>
+            <Backdrop show={showFiltersModal} onClose={onFiltersModalClose}>
+                <ApplicationFilterForm
+                    requirements={requirements}
+                    onClose={onFiltersModalClose}
+                    filters={filters}
+                    setFilters={onFiltersSubmit}
+                />
+            </Backdrop>
         </div>
     );
 };
+
+export async function getServerSideProps(): Promise<GetServerSidePropsResult<ApplicationsProps>> {
+    let requirements = [];
+    try {
+        let { data, error } = await client.query<getAllRequirements, getAllRequirementsVariables>({
+            query: GET_ALL_REQUIREMENTS,
+            variables: {
+                employerId: constants.employerId,
+            },
+        });
+        if (data && !error) requirements = data.getAllRequirements;
+    } catch (e) {
+        console.log(e);
+    }
+
+    return {
+        props: {
+            requirements,
+        },
+    };
+}
 
 export default Applications;
